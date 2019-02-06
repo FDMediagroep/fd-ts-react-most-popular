3a67cd459d1e17e73202309356168266dfc0bd8d
Willem Liu <willem_liu@hotmail.com>
2019-02-06 16:12:33 +0100

    Fixed naming

--- test/MostPopular.test.tsx
+++ test/MostPopular.test.tsx
@@ -2,14 +2,14 @@ import TestRenderer from 'react-test-renderer';
 import MostPopular from '../src/MostPopular';
 import React from 'react';
 
-describe('LatestNews', () => {
+describe('MostPopular', () => {
     test('renders correctly', () => {
         let mostPopular = TestRenderer.create(<MostPopular mostRead={[{uuid: '1', label: 'title van nieuwsbericht', url: 'https://fd.nl'}]} mostComments={[{uuid: '2', label: 'title van nieuwsbericht 2', longread: true, url: 'https://fd.nl'}]}/>);
         expect(mostPopular.toJSON()).toMatchSnapshot();
     });
 
     test('renders is-read correctly', () => {
-        let latestNews = TestRenderer.create(<MostPopular mostRead={[{uuid: '1', isRead: true, label: 'title van nieuwsbericht', url: 'https://fd.nl'}]} mostComments={[{uuid: '2', label: 'title van nieuwsbericht 2', longread: true, url: 'https://fd.nl'}]}/>);
-        expect(latestNews.toJSON()).toMatchSnapshot();
+        let mostPopular = TestRenderer.create(<MostPopular mostRead={[{uuid: '1', isRead: true, label: 'title van nieuwsbericht', url: 'https://fd.nl'}]} mostComments={[{uuid: '2', label: 'title van nieuwsbericht 2', longread: true, url: 'https://fd.nl'}]}/>);
+        expect(mostPopular.toJSON()).toMatchSnapshot();
     });
 });
